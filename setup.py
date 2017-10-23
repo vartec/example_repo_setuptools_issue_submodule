4e8e2fa4043592f76bb40187a172bedfa8f8c25c
Bartek Ogryczak <bartek@eventbrite.com>
2017-10-23 11:08:55 -0700

    ...

--- setup.py
+++ setup.py
@@ -1,23 +1,11 @@
 from setuptools import setup, find_packages
 
-install_requires = [
-    'redis~=2.10.6',
-]
-
-
-tests_require = [
-    'pytest',
-]
-
-
 setup(
-    name='module',
+    name='example_submodule',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=install_requires,
-    tests_require=tests_require,
-    setup_requires=['pytest-runner'],
-    extras_require={
-        'testing': tests_require,
-    },
+    install_requires=[
+        'redis~=2.10.6',
+    ]
+
 )
